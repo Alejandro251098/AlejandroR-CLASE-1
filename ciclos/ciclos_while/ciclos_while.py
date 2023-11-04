53238d7cda6786589774fdf16f4195686bc520b6
Alejandro251098 <alejoramirez19@gmail.com>
2023-11-04 11:57:54 -0500

    tarea clase 2

---
+++ ciclos/ciclos_while/ciclos_while.py
@@ -0,0 +1,58 @@
+# contador = 0
+
+# while contador < 5 :
+#     print ( f"valor del contador: ( contador )")
+#     contador += 1  # tambien sepuedel colocar como contador + 1 ,tener cuidado se debe cerar el ciclo 
+
+# nombres = ["alice" ,"bob" ,"charlie" ,"david"]
+ 
+# for nombre in nombres:
+#      print ( nombre)
+    
+    
+#  bucle for "anidado"
+# for i in range(3):
+#     for j in range(2):
+#         print(f"i",[i], "j",[j])       
+
+
+
+
+
+#ejercicio 1
+
+# ascendente = 0
+# descendente = 11
+# while ascendente < 10 :
+#    ascendente += 1
+#    descendente -= 1
+#    print(ascendente , "/" , descendente) 
+
+
+#ejercicio 2
+
+# import random
+
+# aleatoreo = random.randint( 1,100)
+# adivina = 0
+
+# while adivina != aleatoreo :
+#     if adivina == 0 :
+#         print ( "inicia el juego")
+#     elif adivina < aleatoreo :
+#         print ( "demasiado bajo")
+        
+#     else :
+#         print ("demasaido alto")
+#     adivina = int(input("ingresa el numero"))           
+
+
+
+#ejercicio 3 
+
+
+for " * " in range(5) :
+    for " " in range (1):
+        print(f "*",[*]," ",[ ])
+ 
+   
---
+++ condcionales/else_if.py
@@ -0,0 +1,74 @@
+
+#ejercicio 1
+
+  
+    
+# ejercicio  2
+
+
+# edad = int(input("ingrese sus edad"))
+
+# if edad  < 18 :
+#      print("eres menor de edad")
+        
+# elif edad >= 18 and edad <= 65 :
+#         print("eres un adulto")
+        
+# else:  
+#    print("eres un adulto mayor")  
+   
+   
+
+
+# ejercicio 3
+
+# precio = int(input("ingrese el valor del producto"))
+# edad = int(input("ingrese su edad"))
+
+
+# if edad < 18  :
+#     print(precio*0.9)
+ 
+# elif edad >= 65:
+#     print(precio * 0.85)
+    
+# if edad >=18 and edad < 65 :
+#     print(precio)  
+    
+           
+           
+ #ejercicio 4
+ 
+# nota = int(input("ingrese un numero"))
+ 
+# if nota >= 90 and nota <= 100 : 
+#     print ("A : sobrealiente")
+
+# elif nota >= 80 and nota <= 89 :   
+#     print ("B: bueno")
+# elif nota >=70 and nota <= 79 :
+#     print ("C: satisfactorio")
+# elif nota >= 60 and nota <= 69 :
+#     print("D: necesita mejorar")
+
+# else :
+#    print("F: reprobado")     
+
+
+#ejercicio 5
+
+
+# salario = int ( input("ingrese el valor de su salario anual"))
+
+# if salario <= 10000  :
+#    print  ("no se aplica impuesto")
+# elif salario > 10000 and salario <= 50000 :
+#    print ( salario * 0.1)
+# elif salario > 50000 and salario <= 100000 :
+#     print (salario * 0.2)
+
+# else :
+#     print ( salario * 0.3)          
+        
+
+                     
